e0d5d4a5d1342c30d4444d51598864115d5b3e5b
Sirak <69489773+shizi5742@users.noreply.github.com>
2023-11-27 20:46:35 -0800

    shizi added

---
+++ client/src/app/shizi/shizi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shizi',
+  templateUrl: './shizi.component.html',
+  styleUrls: ['./shizi.component.css']
+})
+export class ShiziComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
